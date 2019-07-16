8230269a3c5a6317da4117eed5c6e8d32524c3a6
Nico Zimmermann <nico.zimmermann@iomedico.com>
2019-07-16 14:43:51 +0200

    current track

--- src/main/java/com/zimmerbell/sonos/page/StatusPage.java
+++ src/main/java/com/zimmerbell/sonos/page/StatusPage.java
@@ -1,11 +1,12 @@
 package com.zimmerbell.sonos.page;
 
 import java.io.IOException;
-import java.util.List;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.model.LambdaModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,7 +16,9 @@ import com.zimmerbell.sonos.model.GroupModel;
 import com.zimmerbell.sonos.model.GroupsModel;
 import com.zimmerbell.sonos.model.HouseholdModel;
 import com.zimmerbell.sonos.model.HouseholdsModel;
+import com.zimmerbell.sonos.pojo.Group;
 import com.zimmerbell.sonos.pojo.Household;
+import com.zimmerbell.sonos.pojo.Track;
 
 public class StatusPage extends AbstractBasePage {
 	private static final long serialVersionUID = 1L;
@@ -45,7 +48,17 @@ public class StatusPage extends AbstractBasePage {
 		});
 
 		form.add(new DropDownChoice<Household>("households", new HouseholdModel(), new HouseholdsModel()));
-		form.add(new DropDownChoice<>("groups", new GroupModel(), new GroupsModel()));
+
+		GroupModel groupModel = new GroupModel();
+		form.add(new DropDownChoice<>("groups", groupModel, new GroupsModel()));
+
+		try {
+			Group group = groupModel.getObject();
+			Track track = group == null ? null : getSonosService().queryPlaybackMetadata(group);
+			form.add(new Label("track", LambdaModel.of(() -> track == null ? null : track.getName())));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 
 //			if (groups.size() > 0) {
 //				Group group = groups.get(0);
