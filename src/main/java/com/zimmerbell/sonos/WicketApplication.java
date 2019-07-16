1133ec5ea24847cb5a593ceffa7be0ac5bec4fad
Nico Zimmermann <nico.zimmermann@iomedico.com>
2019-07-16 15:30:36 +0200

    bootstrap 4

--- src/main/java/com/zimmerbell/sonos/WicketApplication.java
+++ src/main/java/com/zimmerbell/sonos/WicketApplication.java
@@ -1,15 +1,17 @@
 package com.zimmerbell.sonos;
 
 import org.apache.wicket.Page;
-import org.apache.wicket.Session;
 import org.apache.wicket.protocol.http.WebApplication;
-import org.apache.wicket.request.Request;
-import org.apache.wicket.request.Response;
 import org.apache.wicket.settings.RequestCycleSettings.RenderStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.zimmerbell.sonos.page.StatusPage;
+
+import de.agilecoders.wicket.core.Bootstrap;
+import de.agilecoders.wicket.core.settings.BootstrapSettings;
+import de.agilecoders.wicket.webjars.WicketWebjars;
+import de.agilecoders.wicket.webjars.settings.WebjarsSettings;
 
 public class WicketApplication extends WebApplication {
 	private static final Logger log = LoggerFactory.getLogger(WicketApplication.class);
@@ -21,15 +23,13 @@ public class WicketApplication extends WebApplication {
 	@Override
 	protected void init() {
 		super.init();
-		
+
 		// disable version number in url
 		getRequestCycleSettings().setRenderStrategy(RenderStrategy.ONE_PASS_RENDER);
 
-//		WebjarsSettings webjarsSettings = new WebjarsSettings();
-//		WicketWebjars.install(this, webjarsSettings);
-//		// configure bootstrap
-//		BootstrapSettings bootstrapSettings = new BootstrapSettings();
-//		Bootstrap.install(this, bootstrapSettings);
+		WicketWebjars.install(this, new WebjarsSettings());
+		// configure bootstrap
+		Bootstrap.install(this, new BootstrapSettings());
 	}
 
 	@Override
