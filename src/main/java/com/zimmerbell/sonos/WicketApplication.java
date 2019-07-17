383d637849b0dbdb0434686b54f0ce77dfbcf56e
Nico Zimmermann <6852640+nicman42@users.noreply.github.com>
2019-07-17 10:32:59 +0200

    add sonos rest event api

--- src/main/java/com/zimmerbell/sonos/WicketApplication.java
+++ src/main/java/com/zimmerbell/sonos/WicketApplication.java
@@ -5,6 +5,7 @@ import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.settings.RequestCycleSettings.RenderStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.wicketstuff.rest.utils.mounting.PackageScanner;
 
 import com.zimmerbell.sonos.page.StatusPage;
 
@@ -30,11 +31,15 @@ public class WicketApplication extends WebApplication {
 		WicketWebjars.install(this, new WebjarsSettings());
 		// configure bootstrap
 		Bootstrap.install(this, new BootstrapSettings());
+		
+		PackageScanner.scanPackage("com.zimmerbell.sonos.resource");
 	}
-
+	
 	@Override
 	public Class<? extends Page> getHomePage() {
 		return StatusPage.class;
 	}
+	
+	
 
 }
