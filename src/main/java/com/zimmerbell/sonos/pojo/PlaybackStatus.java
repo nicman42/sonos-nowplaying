bea69c258bc7b3a9fb6982f4cdeabdf3ef4e1b73
Nico Zimmermann <6852640+nicman42@users.noreply.github.com>
2019-08-01 16:18:38 +0200

    PlaybackStatus

--- src/main/java/com/zimmerbell/sonos/pojo/PlaybackStatus.java
+++ src/main/java/com/zimmerbell/sonos/pojo/PlaybackStatus.java
@@ -1,11 +1,6 @@
 package com.zimmerbell.sonos.pojo;
 
 public class PlaybackStatus implements IEventType {
-	public static final String PLAYBACK_STATE_PLAYING = "PLAYBACK_STATE_PLAYING";
-	public static final String PLAYBACK_STATE_IDLE = "PLAYBACK_STATE_IDLE";
-	public static final String PLAYBACK_STATE_PAUSED = "PLAYBACK_STATE_PAUSED";
-	public static final String PLAYBACK_STATE_BUFFERING = "PLAYBACK_STATE_BUFFERING";
-
 	private String playbackState;
 
 	public String getPlaybackState() {
@@ -16,13 +11,12 @@ public class PlaybackStatus implements IEventType {
 		this.playbackState = playbackState;
 	}
 
-	public boolean isPlaying() {
-		switch (playbackState) {
-		case PLAYBACK_STATE_PLAYING:
-		case PLAYBACK_STATE_BUFFERING:
-			return true;
-		default:
-			return false;
+	public PlaybackState getPlaybackStateEnum() {
+		try {
+			return PlaybackState.valueOf(playbackState);
+		} catch (IllegalArgumentException e) {
+			return PlaybackState.UNDEFINED;
 		}
 	}
+
 }
