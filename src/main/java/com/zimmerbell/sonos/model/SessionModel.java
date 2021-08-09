37194ae069a1b7d4fa1ecd8a6852ac2b6e364239
Nico Zimmermann <mail@nico-zimmermann.com>
2021-08-09 15:19:39 +0200

    onObjectChanged

--- src/main/java/com/zimmerbell/sonos/model/SessionModel.java
+++ src/main/java/com/zimmerbell/sonos/model/SessionModel.java
@@ -1,17 +1,22 @@
 package com.zimmerbell.sonos.model;
 
 import java.util.Map;
+import java.util.Objects;
 
 import org.apache.wicket.Session;
 import org.apache.wicket.model.IModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.zimmerbell.sonos.pojo.Id;
 
 public abstract class SessionModel<T extends Id> implements IModel<T> {
 	private static final long serialVersionUID = 1L;
 
-	private String objectAttribute;
-	private String mapAttribute;
+	private static final Logger LOG = LoggerFactory.getLogger(SessionModel.class);
+
+	private final String objectAttribute;
+	private final String mapAttribute;
 
 	public SessionModel(String objectAttribute, String mapAttribute) {
 		this.objectAttribute = objectAttribute;
@@ -24,13 +29,24 @@ public abstract class SessionModel<T extends Id> implements IModel<T> {
 	@SuppressWarnings("unchecked")
 	@Override
 	public T getObject() {
-		Map<String, T> map = (Map<String, T>) Session.get().getAttribute(mapAttribute);
-		return map == null ? null : map.get(Session.get().getAttribute(objectAttribute));
+		final Map<String, T> map = (Map<String, T>) Session.get().getAttribute(mapAttribute);
+		final T object = map == null ? null : map.get(Session.get().getAttribute(objectAttribute));
+		return object;
 	}
 
 	@Override
 	public void setObject(T object) {
-		Session.get().setAttribute(objectAttribute, object == null ? null : object.getId());
+		final T oldObject = getObject();
+		if (!Objects.equals(oldObject, object)) {
+			Session.get().setAttribute(objectAttribute, object == null ? null : object.getId());
+
+			LOG.debug("{}: {}", objectAttribute, object == null ? null : object.getId());
+			onObjectChanged(oldObject, object);
+		}
+	}
+
+	protected void onObjectChanged(T oldObject, T object) {
+
 	}
 
 }
